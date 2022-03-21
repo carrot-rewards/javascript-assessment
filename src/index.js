dd95c815d5ccdab8c7f3b028f5fc6374b99fe1a1
Adimas Faisal <adimasfaisal01@gmail.com>
2022-03-21 16:13:32 +0700

    Showing Posts to Front End

--- src/index.js
+++ src/index.js
@@ -21,5 +21,45 @@ async function get_user() {
 async function get_post() {
   var user_id = document.getElementById("user_id").value;
   var _user = await user.get_post(String(user_id));
+  var table = document.getElementById("post-table");
+
   console.log(_user);
+  var header = document.createElement("th");
+  var headId = document.createElement("td");
+  var headIdText = document.createTextNode("Id");
+  var headTitle = document.createElement("td");
+  var headTitleText = document.createTextNode("Title");
+  var headBody = document.createElement("td");
+  var headBodyText = document.createTextNode("Body");
+
+  headId.appendChild(headIdText);
+  headTitle.appendChild(headTitleText);
+  headBody.appendChild(headBodyText);
+
+  header.appendChild(headId);
+  header.appendChild(headTitle);
+  header.appendChild(headBody);
+
+  table.appendChild(header);
+
+  _user.forEach(element => {
+    var row = document.createElement("tr");
+    var y = document.createElement("td");
+    var y1 = document.createTextNode(element.id);
+    var z = document.createElement("td");
+    var z1 = document.createTextNode(element.title);
+    var a = document.createElement("td");
+    var a1 = document.createTextNode(element.body);
+
+    y.appendChild(y1);
+    z.appendChild(z1);
+    a.appendChild(a1);
+
+    row.appendChild(y);
+    row.appendChild(z);
+    row.appendChild(a);
+    
+
+    table.appendChild(row);
+  });
 }
